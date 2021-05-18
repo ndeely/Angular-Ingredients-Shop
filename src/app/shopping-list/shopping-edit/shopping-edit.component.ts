8d29f9423f96bc1c1daa43e9b47d5adff2bc5d29
Niall Deely <nialldeely@gmail.com>
2021-05-18 16:36:52 +0100

    Updated code and dependencies

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -3,7 +3,7 @@ import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { Ingredient } from '@shared/ingredient.model';
 import {ShoppingListService} from "@shopping-list/shopping-list.service";
 import {NgForm} from "@angular/forms";
-import {Subscription} from "rxjs/Subscription";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-edit',
